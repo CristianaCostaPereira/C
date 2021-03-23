e69d3a8d1ceae64524d29bf2e6209f5aaef36253
CristianaCostaPereira <cristiana.vc.pereira@gmail.com>
2021-03-23 16:33:01 +0000

    8th prog (exercises component)

---
+++ Modulo 1/prog0108.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+main()
+{
+    printf("Programa que apresenta os Caracteres Especiais:\n\n");
+    printf("\\n é o enter\n");
+    printf("\\\\ é o caracter barra\n");
+    printf("\\t significa tabulação\n");
+    printf("%%%% símbolo da percentagem\n");
+}
