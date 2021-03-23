3c390eca46a83227f108cb0ed3891d7d1cf0d2ee
CristianaCostaPereira <cristiana.vc.pereira@gmail.com>
2021-03-23 16:05:31 +0000

    6th prog (exercises component)

---
+++ Modulo 1/prog0106.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+
+main() {
+    printf("1 -\tClientes\n"); /* \t to place tabulation, like we pressed the tab key */ 
+    printf("2 -\tFornecedores\n");
+    printf("3 -\tFacturas\n\n"); /* To create the extra line in white */
+    printf("0 -\tSair\n");
+}
