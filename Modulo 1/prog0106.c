d94bbd7256eec0bc2722e894ae5c2414dcfce11b
CristianaCostaPereira <cristiana.vc.pereira@gmail.com>
2021-03-23 16:32:48 +0000

    7th prog (exercises component)

--- Modulo 1/prog0106.c
+++ Modulo 1/prog0106.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 
-main() {
+main()
+{
     printf("1 -\tClientes\n"); /* \t to place tabulation, like we pressed the tab key */ 
     printf("2 -\tFornecedores\n");
     printf("3 -\tFacturas\n\n"); /* To create the extra line in white */
