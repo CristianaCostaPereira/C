4ec086ef62daa3fd387cb8096b00ba18f4ff918f
CristianaCostaPereira <cristiana.vc.pereira@gmail.com>
2021-03-25 18:25:15 +0000

    12th prog (exercises component): buffer

---
+++ Modulo 2/prog0212.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+main()
+{
+    char ch1, ch2;
+
+    printf("Introduza um caracter: ");
+    scanf("%c", &ch1);
+
+    printf("Introduza novo caracter: ");
+    scanf(" %c", &ch2);
+
+    printf("Os caracteres introduzidos foram: '%c' e '%c'.\n", ch1, ch2);
+}
